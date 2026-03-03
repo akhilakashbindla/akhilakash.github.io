25336f7ba7a83c3f7d7feddcad2d8ef35fd9255a
akhilakashbindla <99811783+akhilakashbindla@users.noreply.github.com>
2026-03-03 19:23:31 +0530

    Add utility functions for audio and device detection

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,45 @@
+// js/utils.js - Shared helpers for intro/lab page
 
+// Keystroke sound effect
+const ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+function playKeystroke() {
+  const oscillator = ctx.createOscillator();
+  oscillator.type = 'sine';
+  oscillator.frequency.setValueAtTime(800, ctx.currentTime);
+  const gain = ctx.createGain();
+  gain.gain.setValueAtTime(0.1, ctx.currentTime);
+  oscillator.connect(gain);
+  gain.connect(ctx.destination);
+  oscillator.start();
+  oscillator.stop(ctx.currentTime + 0.05);
+}
+
+// Voice playback
+function playVoice() {
+  const utterance = new SpeechSynthesisUtterance("Decryption complete. Welcome to Akhil Akash Bindla's portfolio.");
+  utterance.rate = 1;
+  utterance.pitch = 1;
+  window.speechSynthesis.speak(utterance);
+}
+
+// Basic device & browser detection
+function getDeviceInfo() {
+  const ua = navigator.userAgent;
+  let os = 'Unknown OS';
+  let browser = 'Unknown Browser';
+
+  if (/windows/i.test(ua)) os = 'Windows';
+  else if (/linux/i.test(ua)) os = 'Linux';
+  else if (/mac/i.test(ua)) os = 'macOS';
+  else if (/android/i.test(ua)) os = 'Android';
+  else if (/iphone|ipad|ipod/i.test(ua)) os = 'iOS';
+
+  if (/chrome/i.test(ua)) browser = 'Chrome';
+  else if (/firefox/i.test(ua)) browser = 'Firefox';
+  else if (/safari/i.test(ua)) browser = 'Safari';
+
+  return { os, browser };
+}
+
+export { playKeystroke, playVoice, getDeviceInfo };
